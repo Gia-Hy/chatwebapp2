a9f50edcca8264660ad35776ee7e37bb8ebaa560
Gia-Hy <87212072+Gia-Hy@users.noreply.github.com>
2022-01-27 17:02:26 -0700

    Add files via upload

---
+++ Kwitter_room.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBxU2pcXDb8C5y_itQ27k6dxzkzGYFls60",
+    authDomain: "yayya-59856.firebaseapp.com",
+    projectId: "yayya-59856",
+    storageBucket: "yayya-59856.appspot.com",
+    messagingSenderId: "922597736759",
+    appId: "1:922597736759:web:afcca9c9eda3ef8eed1ab2"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
